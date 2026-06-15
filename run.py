eed3c7f6c495592d07b260784a8afc11b5b38597
nekkalapsai999-dot <nekkalapsai999@gmail.com>
2026-06-15 13:26:29 +0000

    "15/06/2026"

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ df = pd.read_parquet(
     "data/raw/security_price_history.parquet"
 )
 
+print(df.dtypes)
+
+print(df["Date"].head())
+
 print(df.columns.tolist())
-print(df.shape)
-print(df.head())
